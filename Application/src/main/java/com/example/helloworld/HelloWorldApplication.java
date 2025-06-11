24ffd454592969fbc4df6733e2ea661ed8fa2d29
shaza <shaza@gmail.com>
2025-06-11 04:24:29 +0300

    updating the application source code and Dockerfile

--- Application/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ Application/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class App {
+public class HelloWorldApplication {
 
     @GetMapping("/")
     public String hello() {
@@ -15,6 +15,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }
